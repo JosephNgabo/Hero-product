aad96c28ed54be98008bc14422654edcefc004dc
patricknshimiyimana <patrick.nshimiyimana@codeland.it>
2024-05-16 08:13:28 +0200

    Creation of components to use in our app

---
+++ hero-buy/src/app/environments/environment.pro.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebase: {
+      apiKey: 'AIzaSyDWSZ84_NxdBXmKRzijQ_3zVWOdU87me6o',
+      authDomain: 'hero-buy.firebaseapp.com',
+      projectId: 'hero-buy',
+      storageBucket: 'hero-buy.appspot.com',
+      messagingSenderId: '294296824188',
+      appId: '1:294296824188:web:ac539ffaa839f7759e9cf9',
+      measurementId: 'G-G6N9KXV9NG',
+    },
+  };
